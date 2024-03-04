772f003cd4148c8a7c0b8d519841f4f0625e049f
danielveg4 <dvgilles@gmail.com>
2024-03-04 12:12:06 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import { useState } from 'react';
 import { USERS } from './constants/users';
-import {StyledBox, StyledBoxImage, StyledBoxName, StyledBoxUsername, StyledBoxEmail } from './styles'; 
-
 
 const App = () => {
-	const [currentUser, setCurrentUser] = useState(0);
+	const [usersPerPage, setUsersPerPage] = useState(2);
+	const [currentPage, setCurrentPage] = useState(1);
 
-	const handleNext = (currentUser, setCurrentUser) => {
-		setCurrentUser(currentUser + 1);
-	};
-	
-	const handlePrevius = (currentUser, setCurrentUser) => {
-		setCurrentUser(currentUser - 1);
-	};
-	
+	const { paginatedUsers, totalPages } = paginateUsers(
+		USERS,
+		currentPage,
+		usersPerPage
+	);
+
+	const isFirstPage = currentPage === 1;
+	const isLastPage = currentPage === totalPages;
+
 	return (
 		<>
-			<StyledBox>
-				<StyledBoxImage src={USERS[currentUser].profileImage} alt="foto" />
-				<StyledBoxName>{USERS[currentUser].name}</StyledBoxName>
-				<StyledBoxUsername>{USERS[currentUser].username}</StyledBoxUsername>
-				<StyledBoxEmail>{USERS[currentUser].email}</StyledBoxEmail>
-			</StyledBox>
-			<button onClick={() => handlePrevius(currentUser, setCurrentUser)}>Previous</button>
-			<button onClick={() => handleNext(currentUser, setCurrentUser)}>Next</button>  
+			<h1>Slice de usuarios</h1>
+			{paginatedUsers.map(user => (
+				<div key={user.userId}>
+					<img src={user.profileImage} alt='foto' />
+					<p>{user.name}</p>
+				</div>
+			))}
+			<select
+				value={usersPerPage}
+				onChange={event =>
+					handleSelectChange(setUsersPerPage, event, setCurrentPage)
+				}
+			>
+				<option value='1'>1</option>
+				<option value='2'>2</option>
+				<option value='3'>3</option>
+			</select>
+			<button
+				onClick={() => setCurrentPage(currentPage - 1)}
+				disabled={isFirstPage}
+			>
+				Previous
+			</button>
+			<span>
+				Página {currentPage} de {totalPages}
+			</span>
+			<button
+				onClick={() => setCurrentPage(currentPage + 1)}
+				disabled={isLastPage}
+			>
+				Next
+			</button>
 		</>
 	);
 };
-		
-	
 
+const paginateUsers = (USERS, currentPage, usersPerPage) => {
+	const startIndex = (currentPage - 1) * usersPerPage;
+	const endIndex = startIndex + usersPerPage;
+	const paginatedUsers = USERS.slice(startIndex, endIndex);
+	const totalPages = Math.ceil(USERS.length / usersPerPage);
+	return { paginatedUsers, totalPages };
+};
+
+const handleSelectChange = (setUsersPerPage, event, setCurrentPage) => {
+	setUsersPerPage(event.target.value);
+	setCurrentPage(1);
+};
 
 export default App;
---
+++ src/components/secondBox/SecondBox.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react';
+import { USERS } from './constants/users';
+import {
+	StyledBox,
+	StyledBoxImage,
+	StyledBoxName,
+	StyledBoxUsername,
+	StyledBoxEmail
+} from './styles';
+
+const SecondBox = () => {
+	const [currentUser, setCurrentUser] = useState(0);
+
+	const handleNext = (currentUser, setCurrentUser) => {
+		setCurrentUser(currentUser + 1);
+	};
+
+	const handlePrevius = (currentUser, setCurrentUser) => {
+		setCurrentUser(currentUser - 1);
+	};
+
+	return (
+		<>
+			<StyledBox>
+				<StyledBoxImage src={USERS[currentUser].profileImage} alt='foto' />
+				<StyledBoxName>{USERS[currentUser].name}</StyledBoxName>
+				<StyledBoxUsername>{USERS[currentUser].username}</StyledBoxUsername>
+				<StyledBoxEmail>{USERS[currentUser].email}</StyledBoxEmail>
+			</StyledBox>
+			<button onClick={() => handlePrevius(currentUser, setCurrentUser)}>
+				Previous
+			</button>
+			<button onClick={() => handleNext(currentUser, setCurrentUser)}>
+				Next
+			</button>
+		</>
+	);
+};
+
+export default SecondBox;
